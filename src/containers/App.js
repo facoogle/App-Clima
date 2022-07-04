73a95d30c3543da85b9613f9818c4c4d16b46f73
facoogle <facoogle@gmail.com>
2022-07-04 14:36:02 +0200

    Hola

--- src/containers/App.js
+++ src/containers/App.js
@@ -110,13 +110,13 @@ useEffect(() => {
  
 
 
-  useEffect(() => {
-    onSearch(rosario);
-    onSearch(buenosaires);
-    onSearch(cordoba)
-    
-    
-  }, []); 
+  //useEffect(() => {
+    //onSearch(rosario);
+    //onSearch(buenosaires);
+    //onSearch(cordoba)
+   // 
+  ///  
+ // }, []); 
 
   
 
